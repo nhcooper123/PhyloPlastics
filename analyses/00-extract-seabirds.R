dd6649d92618517b268070a8e7ac1151503fd283
Natalie Cooper <natalie.cooper@nhm.ac.uk>
2024-08-29 10:15:30 +0100

    Tidy script

--- analyses/00-extract-seabirds.R
+++ analyses/00-extract-seabirds.R
@@ -1,7 +1,21 @@
-# Extract seabirds from birldlife taxonomy
+# Extract seabirds from Bird Life taxonomy
+# HBW v8.1
+# Available at: http://datazone.birdlife.org/userfiles/file/Species/Taxonomy/HBW-BirdLife_Checklist_v81_Jan24.zip
+# Natalie Cooper Sept 2024. 
+# -----------------------------------------
 
-# Seabirds = Sphenisciformes, Procellariiformes, Pelecaniformes, Charadriiformes, Gaviiformes,Anseriformes; Anatidae; Mergini
+# Seabirds are defined as follows
+# 1) All Sphenisciformes (penguins)
+# 2) All Procellariiformes (petrels, albatross etc.)
+# 3) All Gaviiformes (divers/loons)
+# 4) All Phaethontiformes (tropicbirds)
+# 5) All Suliformes (gannets, boobies etc.) except Anghingidae (anhingas)
+# 6) Within Pelecaniformes, Pelecanidae (pelicans)
+# 7) Within Charadriiformes, Laridae (gulls), Alcidae (auks), Stercorariidae (skuas) and Phalaropus (phalaropes)
+# 8) Within Anserifromes, Mergini (diving ducks)
+# We also exclude the extinct species Camptorhynchus labradorius and Hydrobates macrodactylus
 
+# -----------------------------------------
 # Load libraries
 library(readxl)
 library(tidyverse)
@@ -20,7 +34,7 @@ birds <-
 seabirds <- 
   birds %>%
   # Split species name into Genus and species
-  separate(`Scientific name`, "Genus", "Species", remove = FALSE) %>%
+  separate(`Scientific name`, c("Genus", "Species"), remove = FALSE) %>%
   # Select orders/families/tribes/genera containing seabirds
   filter(Order == "SULIFORMES" | 
          Order ==  "SPHENISCIFORMES" | 
@@ -32,9 +46,27 @@ seabirds <-
            `Family name` == "Stercorariidae" |
            `Family name` == "Laridae" |
            Tribe == "Mergini" |
-           Genus == "Phalaropus") %>%
+           Tribe == "Phalaropodini") %>%
   # Remove Anhingidae from Suliformes
-  filter(Family != "Anhingidae")
+  filter(`Family name` != "Anhingidae") %>%
+  # Remove two extinct species
+  filter(`Scientific name` != "Camptorhynchus labradorius" &
+         `Scientific name` != "Hydrobates macrodactylus") %>%
+  # select only the required columns and tidy up names a bit
+  dplyr::select(Order, Family = `Family name`, Subfamily, Tribe, 
+                Common_name = `Common name`, Binomial = `Scientific name`, 
+                Genus, IUCN_2023 = `2023 IUCN Red List category`,
+                Synonyms)
+# Save the list
+write_csv(seabirds, file = "data-raw/seabird-list-HBW.csv")
 
-  old <- read.csv("data-raw/2022-10-02  Data S1. Behavioural and life-history dataset.YesNo.csv")
-  
+#---------------------
+# Summarise the data
+# -------------------
+
+# By order
+seabirds %>% group_by(Order) %>% summarise(n())
+# By family
+seabirds %>% group_by(Family) %>% summarise(n())
+# Total
+seabirds %>% summarise(n())
