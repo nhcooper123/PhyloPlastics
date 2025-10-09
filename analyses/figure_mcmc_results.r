93369c7f4d6b69d394df32487915a88b00687bed
Natalie Cooper <natalie.cooper@nhm.ac.uk>
2025-10-09 16:57:16 +0100

    Add dotted line using after_scale

--- analyses/figure_mcmc_results.r
+++ analyses/figure_mcmc_results.r
@@ -39,22 +39,28 @@ effects_df <-
                                                        "scavenging", "skimming", "surface_seizing", "terrestrial_feeding",
                                                        "body_mass_2sd", "pelagic_specialist"))))      
 
-rectangles <- data.frame(X = c(min(effects_df$HPD.lower)-1, max(effects_df$HPD.upper)+1))
+rectangles <- data.frame(X = c(min(effects_df$HPD.lower)-1, max(effects_df$HPD.upper)+1),
+                         Y = c("other_inverts", "as_refuse"))
+
 
 # Plot
-ggplot(effects_df, aes(x = post.mean, y = as.factor(predictor))) +
+ggplot(effects_df, aes(x = post.mean, y = predictor)) +
   geom_point(size = 1.5) +
-  geom_errorbarh(aes(xmin = HPD.lower, xmax = HPD.upper), height = 0.2) +
+  geom_errorbarh(aes(xmin = HPD.lower, xmax = HPD.upper), width = 0.2) +
   geom_vline(xintercept = 0, linetype = "dashed") +
   xlab("posterior mean") +
   ylab("") +
   theme_bw() +
-  geom_area(data = rectangles, aes(x=X,y="other_inverts"),alpha=0.3,fill="red") +
-  geom_area(data = rectangles, aes(x=X,y="as_refuse"),alpha=0.3,fill="blue") +
-  geom_area(data = rectangles, aes(x=X,y="terrestrial_feeding"),alpha=0.3,fill="green") +
-  coord_cartesian(xlim = c(min(effects_df$HPD.lower), max(effects_df$HPD.upper)))
+  #geom_area(data = rectangles, aes(x=X,y="as_agriculture"),alpha=0.1,fill="grey") +
+  #geom_area(data = rectangles, aes(x=X,y="as_refuse"),alpha=0.2,fill="grey") +
+  #geom_area(data = rectangles, aes(x=X,y="body_mass_2sd"),alpha=0.3,fill="grey") +
+  coord_cartesian(xlim = c(min(effects_df$HPD.lower), max(effects_df$HPD.upper))) +
+  geom_hline(aes(yintercept = stage(predictor, after_scale = yintercept - 0.5)),
+             data = data.frame(predictor = "other_inverts"),
+             linetype = "dotted")
 
-# Save effect size plot
-ggsave("effect.size.plot.png", effect.size.plot, width = 180, height = 140, 
+  
+  # Save effect size plot
+#ggsave("effect.size.plot.png", effect.size.plot, width = 180, height = 140, 
        units = "mm", dpi = 300)
 
